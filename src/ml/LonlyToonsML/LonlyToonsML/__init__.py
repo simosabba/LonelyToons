d678998a16abecb1df6a4815b46b169f22f42d93
simone sabba (lenovo) <simone.sabba@live.com>
2018-10-21 02:20:10 +0200

    aa

--- src/ml/LonlyToonsML/LonlyToonsML/__init__.py
+++ src/ml/LonlyToonsML/LonlyToonsML/__init__.py
@@ -4,13 +4,15 @@ The flask application package.
 
 from flask import Flask
 from flask_restplus import Resource, Api
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 api = Api(app)
 
 @api.route('/analyze')
 class Analyers(Resource):
-    def get(self):
+    def post(self):
         return {'status': 'sad'}
 
 import LonlyToonsML.views
--- src/ml/LonlyToonsML/readme.txt
+++ src/ml/LonlyToonsML/readme.txt
@@ -1,1 +1,4 @@
+
+pip freeze > requirements.txt
+
 python.exe -m pip install --upgrade -r d:\home\site\wwwroot\requirements.txt
